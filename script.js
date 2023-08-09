9e8c37dd274f8eb2e3107c71b5b52a9748938f42
Mohammedshajeeh <140877532+Mohammedshajeeh@users.noreply.github.com>
2023-08-09 21:10:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function message(){
+    var name1=document.getElementById('name').value
+    var email1=document.getElementById('email').value
+    var number1=document.getElementById('number').value
+    var textera1=document.getElementById('textera'),value
+    const success1=document.getElementById('success').value
+    const danger1=document.getElementById('danger').value
+
+    if(name1.value =='' || email1.value =='' || number1.value == '' || textera1.value == ''){
+       alert('not successfully')
+    }else{
+        alert('successfully')
+
+   
+    }
+   
+}
+      
